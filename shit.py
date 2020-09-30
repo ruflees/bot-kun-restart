f0008382df52232489aaec48ef9126a5deee8718
ruflees <66584935+ruflees@users.noreply.github.com>
2020-09-30 02:26:10 -0300

    something

--- shit.py
+++ shit.py
@@ -1,24 +1,5 @@
-import discord
-from discord.ext import commands
+test = "test"
 
-client = commands.Bot(command_prefix='#')
-
-@client.event
-async def on_ready():
-    print("bot is ready")
-
-
-def member_cleaner(lst):
-    members_cleaned = []
-    member_str = '' + str(lst)
-    member_splitted = member_str.split('#')
-    for x in range(0, int(len(member_splitted)),2):
-        members_cleaned.append(member_splitted[x])
-    return members_cleaned
-
-@client.command()
-async def test(ctx, *, member:discord.Member=None):
-    await ctx.send(member.id)
-
-
-client.run('NzI5MjI4NDk3NjY2NTcyMzYw.XwF49g.CgFbFirFkMFFqPkd4KcmW35G2eE')
+test_file = open("%s"%(test), "a")
+test_file.write(test)
+test_file.close()
